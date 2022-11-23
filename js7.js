ddc1bf11c03865a137da08b262bebc822227b1b1
yugi1405 <yuge1405@gmail.com>
2022-11-23 12:20:53 +0530

    add files.

---
+++ js7.js
@@ -0,0 +1,8 @@
+var apq=prompt("enter avg monthly income of p and q");
+var aqr=prompt("enter avg monthly income of q and r");
+var apr=promt("enter avg monthly income of p and r");
+ap=parseFloat(apq);
+aqr=parseFloat(aqr);
+apr=parseFloat(apr);
+var p=((apq*2+aqr*2+apr*2)/2-(aqr*2));
+alert(p);
